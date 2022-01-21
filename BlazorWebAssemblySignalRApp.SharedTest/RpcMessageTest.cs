72a0fe7ee8fc9657d986becc250d580458a0904c
Simone Giacomelli <simone.giacomelli@gmail.com>
2022-01-21 20:35:53 +0100

    some dispatcher

--- BlazorWebAssemblySignalRApp.SharedTest/RpcMessageTest.cs
+++ BlazorWebAssemblySignalRApp.SharedTest/RpcMessageTest.cs
@@ -4,84 +4,132 @@ using System.Reflection;
 using System.Text.Json;
 using System.Threading.Tasks;
 using BlazorWebAssemblySignalRApp.Shared.Rpc;
+using Interface1;
 using NUnit.Framework;
 
-namespace BlazorWebAssemblySignalRApp.SharedTest;
-
-[TestFixture]
-public class RpcMessageTest
+namespace BlazorWebAssemblySignalRApp.SharedTest
 {
-    [Test]
-    public void test1()
+    [TestFixture]
+    public class RpcMessageTest
     {
-        var msg = RpcMessage.Encode("foo", "bar");
-        var restored = RpcMessage.Decoder(msg);
-        Assert.AreEqual("foo", restored.Name);
-        Assert.AreEqual("bar", restored.Payload);
+        [Test]
+        public void test1()
+        {
+            var msg = RpcMessage.Encode("foo", "bar");
+            var restored = RpcMessage.Decoder(msg);
+            Assert.AreEqual("foo", restored.Name);
+            Assert.AreEqual("bar", restored.Payload);
+        }
+    }
+
+    [TestFixture]
+    public class RpcSingleInstanceExchangeTest
+    {
+        class SumRequest : IRequest<SumResponse>
+        {
+            public int a { get; set; }
+            public int b { get; set; }
+        }
+
+        class SumResponse
+        {
+            public int Result { get; set; }
+        }
+
+        [Test]
+        public async Task SingleInstanceTest()
+        {
+            ContextHandlers<Object> contextHandlers = new();
+            contextHandlers.Register((SumRequest req, Object context) => new SumResponse { Result = req.a + req.b });
+
+            var result = await BlazorWebAssemblySignalRApp.Shared.Rpc.Consumer
+                .Send<SumRequest, SumResponse>(new SumRequest { a = 40, b = 2 }
+                    , async (name, payload) =>
+                    {
+                        Console.WriteLine($"name={name}");
+                        Console.WriteLine($"payload={payload}");
+                        var dispatch = contextHandlers.Dispatch(name, payload, new());
+                        return dispatch;
+                    });
+            Assert.AreEqual(42, result.Result);
+        }
+    }
+
+    [TestFixture]
+    public class RpcInterfaceServiceTest
+    {
+        [Test]
+        public async Task IntefaceTest()
+        {
+            var provider = new Provider();
+            var disp = new Dispatcher();
+
+            disp.Register<ICalculator>();
+
+
+            Func<string, string, Task<string>> dispatcher = async (name, payload) =>
+            {
+                Console.WriteLine($"dispatcher name={name} payload={payload}");
+                var res = JsonSerializer.Serialize(16, typeof(int));
+                var restore = JsonSerializer.Deserialize(res, typeof(int));
+                return res;
+            };
+            var power2 = RpcClient.Create<ICalculator>(dispatcher).Power(4);
+            var result = await power2;
+            Assert.AreEqual(16, result);
+        }
     }
 }
 
-[TestFixture]
-public class RpcSingleInstanceExchangeTest
-{
-    class SumRequest : IRequest<SumResponse>
-    {
-        public int a { get; set; }
-        public int b { get; set; }
-    }
-
-    class SumResponse
-    {
-        public int Result { get; set; }
-    }
-
-    [Test]
-    public async Task SingleInstanceTest()
-    {
-        ContextHandlers<Object> contextHandlers = new();
-        contextHandlers.Register((SumRequest req, Object context) => new SumResponse { Result = req.a + req.b });
-
-        var result = await BlazorWebAssemblySignalRApp.Shared.Rpc.Consumer
-            .Send<SumRequest, SumResponse>(new SumRequest { a = 40, b = 2 }
-                , async (name, payload) =>
-                {
-                    Console.WriteLine($"name={name}");
-                    Console.WriteLine($"payload={payload}");
-                    var dispatch = contextHandlers.Dispatch(name, payload, new());
-                    return dispatch;
-                });
-        Assert.AreEqual(42, result.Result);
-    }
-}
-
-[TestFixture]
-public class RpcInterfaceServiceTest
+namespace Interface1
 {
     interface ICalculator
     {
-        public Task<int> Sum(int a, int b);
-        public Task<int> Power2(int a);
+        public Task<int> Power(int a);
     }
+}
 
-    class Provider : ICalculator
+
+namespace Interface2
+{
+    interface ICalculator
     {
-        public async Task<int> Sum(int a, int b) => a + b;
-        public async Task<int> Power2(int a) => a * a;
+        public Task<int> Power(int a);
     }
+}
 
+
+namespace Impl1
+{
+    class Provider : Interface1.ICalculator
+    {
+        public async Task<int> Power(int a) => a * a;
+    }
+}
+
+namespace Impl2
+{
+    class Provider : Interface2.ICalculator
+    {
+        public async Task<int> Power(int a) => a * a;
+    }
+}
+
+[TestFixture]
+public class DispatcherTest
+{
     [Test]
-    public async Task IntefaceTest()
+    public async Task TestGetName()
     {
-        Func<string, string, Task<string>> dispatcher = async (name, payload) =>
-        {
-            Console.WriteLine($"dispatcher name={name} payload={payload}");
-            var res = JsonSerializer.Serialize(16, typeof(int));
-            var restore = JsonSerializer.Deserialize(res, typeof(int));
-            return res;
-        };
-        var power2 = RpcClient.Create<ICalculator>(dispatcher).Power2(4);
-        var result = await power2;
-        Assert.AreEqual(16, result);
+        var name1 = Dispatcher.GetNameFromType<Interface1.ICalculator>();
+        var name2 = Dispatcher.GetNameFromType<Interface2.ICalculator>();
+
+        var dispatcher = new Dispatcher();
+        dispatcher.Register<Interface1.ICalculator>();
+        dispatcher.Register<Interface2.ICalculator>();
+
+        Assert.AreEqual(typeof(Interface1.ICalculator), dispatcher.GetTypeFromName(name1));
+        Assert.AreEqual(typeof(Interface2.ICalculator), dispatcher.GetTypeFromName(name2));
     }
 }
 
---
+++ Shared/Rpc/Dispatcher.cs
@@ -0,0 +1,18 @@
+namespace BlazorWebAssemblySignalRApp.Shared.Rpc;
+
+public class Dispatcher
+{
+    private Dictionary<String, Type> dict = new();
+
+    public void Register<T>()
+    {
+        dict[GetNameFromType<T>()] = typeof(T);
+    }
+
+    public Type GetTypeFromName(string name)
+    {
+        return dict[name];
+    }
+
+    public static string GetNameFromType<T>() => typeof(T).FullName!;
+}
